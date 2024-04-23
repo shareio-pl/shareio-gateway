accf3cefd0b8b7cd0ced70331c18704a533c833c
Sedarius-1 <jakubkalinowski1234@gmail.com>
2024-04-23 13:29:12 +0200

    Added routing for offers and addresses

--- src/main/java/org/shareio/gateway/GatewayConfig.java
+++ src/main/java/org/shareio/gateway/GatewayConfig.java
@@ -4,7 +4,6 @@ package org.shareio.gateway;
 import lombok.Getter;
 import org.shareio.gateway.config.AuthFilter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
